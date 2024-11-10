62385273c9869a9859fc4b2e3eb932901f4e8741
Nikhil Dahake <nikhildahake@hotmail.com>
2024-11-10 19:22:41 +0530

    Add OR operator for multiple delimiters

--- src/main/com/incubyte/Calculator.java
+++ src/main/com/incubyte/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator {
 			return 0;
 		}
 		else if (input.contains(",")) {
-			String arr[] = input.split(",");
+			String arr[] = input.split(",|\n");
 			int sum = 0;
 			
 			for (int i=0; i<arr.length; i++) {
